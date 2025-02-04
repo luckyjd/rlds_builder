94581b19adc492c6a4c1b8387fe69441d3865fcb
uran <uran@gmail.com>
2025-02-04 16:05:54 +0700

    v1.7.2 update

--- rh20t/rh20t_dataset_builder.py
+++ rh20t/rh20t_dataset_builder.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import re
 import json
 import numpy as np
 from logger import get_logger
@@ -179,10 +180,12 @@ class Rh20tDataset(tfds.core.GeneratorBasedBuilder):
         if not cfg_folder:
             raise ValueError("can not found cam dir")
         scene_folders = glob.glob(os.path.join(cfg_folder, "task_*_user_*_scene_*_cfg_*"))
+        pattern = re.compile(r"task_\d+_user_\d+_scene_\d+_cfg_\d+$")
+        filtered_folders = [folder for folder in scene_folders if pattern.search(os.path.basename(folder))]
+        split_index = int(len(filtered_folders) * train_percent)
 
-        split_index = int(len(scene_folders) * train_percent)
-        train_cfg = scene_folders[:split_index]
-        test_cfg = scene_folders[split_index:]
+        train_cfg = filtered_folders[:split_index]
+        test_cfg = filtered_folders[split_index:]
 
         return {
             "train": self._generate_examples(train_cfg),
